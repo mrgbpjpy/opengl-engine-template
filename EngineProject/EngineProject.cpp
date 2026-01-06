dba2b17b9f4e6361787e49ea02a0df67027a3db0
mrgbpjpy <mrgbpjpy@gmail.com>
2026-01-05 23:04:29 -0500

    OpenGL with CMAKE Template

---
+++ EngineProject/EngineProject.cpp
@@ -0,0 +1,12 @@
+﻿// EngineProject.cpp : Defines the entry point for the application.
+//
+
+#include "EngineProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
